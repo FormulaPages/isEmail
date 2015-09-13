ef5d19185c3c82a431e5436f7361c09e9eaa6113
Peter Moresi <peter.moresi@gmail.com>
2015-09-12 17:00:37 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+(function (global, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(["exports", "module"], factory);
+    } else if (typeof exports !== "undefined" && typeof module !== "undefined") {
+        factory(exports, module);
+    } else {
+        var mod = {
+            exports: {}
+        };
+        factory(mod.exports, mod);
+        global.ISEMAIL = mod.exports;
+    }
+})(this, function (exports, module) {
+    "use strict";
+
+    module.exports = ISEMAIL;
+
+    function ISEMAIL(email) {
+        var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        return re.test(email);
+    }
+
+    ;
+});
